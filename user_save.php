dbd17c6cc24adee65246b8593961a731cd34d1de
romisbbx <romis.bbx@gmail.com>
2013-10-04 18:42:12 +0400

    Таблица исключений юзеров

--- user_save.php
+++ user_save.php
@@ -16,26 +16,38 @@ class App {
 		));;
 
 		$this->setting = $this->db->getRow('SELECT * FROM setting WHERE id = 1');
+		$this->ban = $this->db->getCol('SELECT id FROM ban');
 	}
 
 	function add_to_base() {
 		foreach ($_POST as $key => $value) {
-			$ins = array(
-				'id' => $key,
-				'like' => isset($value['l']) ? $value['l'] : 0,
-				'repost' => isset($value['r']) ? $value['r'] : 0,
-				'comment' => isset($value['c']) ? $value['c'] : 0,
-				'topic' => isset($value['t']) ? $value['t'] : 0,
-				'photo' => isset($value['p']) ? $value['p'] : 0
-			);
+			$baned = false;
 
-			$ins['rating'] = $ins['like'] * $this->setting['like']
-				+ $ins['repost'] * $this->setting['repost']
-				+ $ins['comment'] * $this->setting['comment']
-				+ $ins['topic'] * $this->setting['topic']
-				+ $ins['photo'] * $this->setting['photo'];
+			foreach ($this->ban as $ban_item) {
+				if ($ban_item == $key) {
+					$baned = true;
+					break;
+				}
+			}
 
-			$this->db->query('INSERT INTO user SET ?u', $ins);
+			if (!$baned) {
+				$ins = array(
+					'id' => $key,
+					'like' => isset($value['l']) ? $value['l'] : 0,
+					'repost' => isset($value['r']) ? $value['r'] : 0,
+					'comment' => isset($value['c']) ? $value['c'] : 0,
+					'topic' => isset($value['t']) ? $value['t'] : 0,
+					'photo' => isset($value['p']) ? $value['p'] : 0
+				);
+
+				$ins['rating'] = $ins['like'] * $this->setting['like']
+					+ $ins['repost'] * $this->setting['repost']
+					+ $ins['comment'] * $this->setting['comment']
+					+ $ins['topic'] * $this->setting['topic']
+					+ $ins['photo'] * $this->setting['photo'];
+
+				$this->db->query('INSERT INTO user SET ?u', $ins);
+			}
 		}
 	}
 }
